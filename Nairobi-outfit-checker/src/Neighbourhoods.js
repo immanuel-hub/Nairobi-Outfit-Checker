06aaaa556b7de188841e324316af975192db9d88
immanuel <immanuel.okoth@student.moringaschool.com>
2026-05-14 14:19:23 +0300

    added a Neighbourhood.js file where I looked up at gps coordinates of different places and wrote them down to be used by the open-meteo weather API to get forecast for the locations

---
+++ Nairobi-outfit-checker/src/Neighbourhoods.js
@@ -0,0 +1,10 @@
+export const NEIGHBOURHOODS = [
+   { name: 'CBD', lat: -1.2833, lon: 36.8167 }, 
+  { name: 'Westlands', lat: -1.2676, lon: 36.8108 }, 
+  { name: 'Karen', lat: -1.3182, lon: 36.7102 }, 
+  { name: 'Eastlands',lat: -1.2833, lon: 36.8667 }, 
+  { name: 'Kasarani', lat: -1.2167, lon: 36.8833 }, 
+  { name: 'Kilimani', lat: -1.2900, lon: 36.7800 }, 
+  { name: 'Langata',  lat: -1.3500, lon: 36.7333 }, 
+  { name: 'Thika Road', lat: -1.2000, lon: 36.8833 }, 
+]
