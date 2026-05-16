dc9443ee4887c31dc6f701b32956e6f94f0318ac
immanuel <immanuel.okoth@student.moringaschool.com>
2026-05-16 15:03:39 +0300

    fix: add base URL for GitHub pages

--- Nairobi-outfit-checker/vite.config.js
+++ Nairobi-outfit-checker/vite.config.js
@@ -3,6 +3,7 @@ import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
 
 export default defineConfig({
+  base: '/Nairobi-Outfit-Checker/',
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
